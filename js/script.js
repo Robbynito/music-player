b36090362cc94fd5e297710fbded91032740a548
Robbynito <125513796+Robbynito@users.noreply.github.com>
2023-05-15 13:06:01 -0700

    New Margin

--- js/script.js
+++ js/script.js
@@ -1,109 +1,109 @@
 const songs = [
   {
-   title: "Black",
-   artist: "Okkervil River",
-   image:`url("../assets/Black Cover.png")`,
-   song: "../assets/songs/Black.mp3",
-   color: "#BB9203C"
+    title: "Black",
+    artist: "Okkervil River",
+    image: `url("../assets/Black Cover.png")`,
+    song: "../assets/songs/Black.mp3",
+    color: "#BB9203C"
   },
   {
     title: "Dancing",
     artist: "Sonic Sea Turtles",
-    image:`url("../assets/Dancing Cover.png")`,
+    image: `url("../assets/Dancing Cover.png")`,
     song: "assets/songs/Dancing.mp3",
     color: "#06C367"
-   },
-   {
+  },
+  {
     title: "Happiness In Liquid Form",
     artist: "Alfie Templeton",
-    image:`url("../assets/Happiness In Liquid Form Cover.png")`,
+    image: `url("../assets/Happiness In Liquid Form Cover.png")`,
     song: "assets/songs/Happiness In Liquid Form.mp3",
     color: "#D84295"
-   },
-   {
+  },
+  {
     title: "I Needed Love",
     artist: "Under the Rug",
-    image:`url("../assets/I Needed Love Cover.png")`,
+    image: `url("../assets/I Needed Love Cover.png")`,
     song: "assets/songs/I Needed Love.mp3",
     color: "#C6B196"
-   },
-   {
+  },
+  {
     title: "Lose Your Soul",
     artist: "Dead Man's Bones",
-    image:`url("../assets/Lose Your Soul Cover.png")`,
+    image: `url("../assets/Lose Your Soul Cover.png")`,
     song: "assets/songs/Lose Your Soul.mp3",
     color: "#320104"
-   },
-   {
+  },
+  {
     title: "Low",
     artist: "egg",
-    image:`url("../assets/Low Cover.png")`,
+    image: `url("../assets/Low Cover.png")`,
     song: "assets/songs/Low.mp3",
     color: "#21031F"
-   },
-   {
+  },
+  {
     title: "Medicine",
     artist: "Matt Berry",
-    image:`url("../assets/Medicine Cover.png")`,
+    image: `url("../assets/Medicine Cover.png")`,
     song: "assets/songs/Medicine.mp3",
     color: "#2F2A26"
-   },
-   {
+  },
+  {
     title: "Notre Dame",
     artist: "Paris Paloma",
-    image:`url("../assets/Notre Dame Cover.png")`,
+    image: `url("../assets/Notre Dame Cover.png")`,
     song: "assets/songs/notre dame.mp3",
     color: "#050E2B"
-   },
-   {
+  },
+  {
     title: "Photo ID",
     artist: "Remi Wolf and Dominick Fike",
-    image:`url("../assets/Photo ID Cover.png")`,
+    image: `url("../assets/Photo ID Cover.png")`,
     song: "assets/songs/Photo ID.mp3",
     color: "#FE2920"
-   },
-   {
+  },
+  {
     title: "Running Up That Hill",
     artist: "Meg Meyers",
-    image:`url("../assets/Running Up That Hill Cover.png")`,
+    image: `url("../assets/Running Up That Hill Cover.png")`,
     song: "assets/songs/Running Up That Hill.mp3",
     color: "#C1BAA7"
-   },
-   {
+  },
+  {
     title: "Rx",
     artist: "Your Neighbors",
-    image:`url("../assets/Rx Cover.png")`,
+    image: `url("../assets/Rx Cover.png")`,
     song: "assets/songs/Rx.mp3",
     color: "#8AADB8"
-   },
-   {
+  },
+  {
     title: "Silver Platters",
     artist: "Les Gold",
-    image:`url("../assets/Silver Platters Cover.png")`,
+    image: `url("../assets/Silver Platters Cover.png")`,
     song: "assets/songs/Silver Platters.mp3",
     color: "#B2B9C4"
-   },
-   {
+  },
+  {
     title: "The Love Club",
     artist: "Lorde",
-    image:`url("../assets/The Love Club Cover.png")`,
+    image: `url("../assets/The Love Club Cover.png")`,
     song: "assets/songs/The Love Club.mp3",
     color: "#A86062"
-   },
-   {
+  },
+  {
     title: "The Story I Heard",
     artist: "Blind Pilot",
-    image:`url("../assets/The Story I Heard Cover.png")`,
+    image: `url("../assets/The Story I Heard Cover.png")`,
     song: "assets/songs/The Story I Heard.mp3",
     color: "#142B35"
-   },
-   {
+  },
+  {
     title: "Visitor",
     artist: "Of Monsters and Men",
-    image:`url("../assets/Visitor Cover.png")`,
+    image: `url("../assets/Visitor Cover.png")`,
     song: "assets/songs/Visitor.mp3",
     color: "#1E1E1E"
-   }
+  }
 ];
 const songTitle = document.querySelector('#songTitle');
 const artist = document.querySelector('#artist');
@@ -112,7 +112,7 @@ const bgrdColor = document.querySelector('body')
 
 songTitle.innerHTML = "Test.";
 artist.innerHTML = "Test";
-albumCover.style.backgroundImage= 'url(../assets/mountain.jpg)';
+albumCover.style.backgroundImage = 'url(../assets/mountain.jpg)';
 
 /*
  * Navigate to Items with Event Listeners
@@ -132,9 +132,9 @@ const randButton = document.querySelector("img.random");
 const record = document.querySelector("#record");
 
 function recordRotate() {
-  if (isPlaying==true){
-    rotations +=1,
-    record.style.transform = "rotate("+rotations+"deg)"
+  if (isPlaying == true) {
+    rotations += 1,
+      record.style.transform = "rotate(" + rotations + "deg)"
   }
 }
 
@@ -142,9 +142,9 @@ function recordRotate() {
 nextButton.addEventListener("click", () => {
   // update, then increase item number with each click
   itemNumber = itemNumber + 1;
-  
+
   // if at LAST item
-  if ( itemNumber > songs.length -1 ) {
+  if (itemNumber > songs.length - 1) {
     // reset counting to first item
     itemNumber = 0;
   }
@@ -156,20 +156,20 @@ nextButton.addEventListener("click", () => {
   bgrdColor.style.backgroundColor = songs[itemNumber].color;
   currentSong.src = songs[itemNumber].song;
   currentSong.play();
-  record.style.transform = "rotate("+0+"deg)"
+  record.style.transform = "rotate(" + 0 + "deg)"
 });
-  
+
 // listen for clicks on previous button
 prevButton.addEventListener("click", () => {
   // update, then DEcrease item number with each click
   itemNumber = itemNumber - 1;
-  
+
   // if at FIRST item
-  if ( itemNumber < 0 ) {
+  if (itemNumber < 0) {
     // reset counting to last item
-    itemNumber = songs.length -1;
-  }  
-  
+    itemNumber = songs.length - 1;
+  }
+
   // display data based on array index
   songTitle.innerHTML = songs[itemNumber].title;
   artist.innerHTML = songs[itemNumber].artist;
@@ -177,93 +177,93 @@ prevButton.addEventListener("click", () => {
   bgrdColor.style.backgroundColor = songs[itemNumber].color;
   currentSong.src = songs[itemNumber].song;
   currentSong.play();
-  record.style.transform = "rotate("+0+"deg)"
+  record.style.transform = "rotate(" + 0 + "deg)"
 });
 
 // listen for clicks on random button
 randButton.addEventListener('click', () => {
-  
+
   // generate random number, for total number, of items (data)
-  randomNumber = Math.floor ( Math.random() * songs.length );
-  
+  randomNumber = Math.floor(Math.random() * songs.length);
+
   // display data based random number
-  songTitle.innerHTML = songs[randomNumber].title;
-  artist.innerHTML = songs[randomNumber].artist;
-  albumCover.style.backgroundImage = songs[randomNumber].image;
+  songTitle.innerHTML = songs[itemNumber].title;
+  artist.innerHTML = songs[itemNumber].artist;
+  albumCover.style.backgroundImage = songs[itemNumber].image;
   bgrdColor.style.backgroundColor = songs[itemNumber].color;
   currentSong.src = songs[itemNumber].song;
   currentSong.play();
-  record.style.transform = "rotate("+0+"deg)"
-  
+  record.style.transform = "rotate(" + 0 + "deg)"
+
 });
 
 // listen for keypress next
 document.addEventListener('keyup', (event) => {
-  
+
   // check-check: What key was pressed?
   // console.log(event);
 
   // Next Right Arrow  
-  if ( event.key === "ArrowRight" ) {
-   
-   // update, then increase item number with each click
-  itemNumber = itemNumber + 1;
-  
-  // if at LAST item
-  if ( itemNumber > songs.length -1 ) {
-    // reset counting to first item
-    itemNumber = 0;
-  }
+  if (event.key === "ArrowRight") {
 
-  // display data based on array index
-  songTitle.innerHTML = songs[itemNumber].title;
-  artist.innerHTML = songs[itemNumber].artist;
-  albumCover.style.backgroundImage = songs[itemNumber].image;
-  bgrdColor.style.backgroundColor = songs[itemNumber].color;
-  currentSong.src = songs[itemNumber].song;
-  currentSong.play();
-  record.style.transform = "rotate("+0+"deg)"
-    
-  }
-  
-  // Prev Left Arrow
-  if ( event.key === "ArrowLeft" ) {
-  
-   // update, then DEcrease item number with each click
-  itemNumber = itemNumber - 1;
-  
-  // if at FIRST item
-  if ( itemNumber < 0 ) {
-    // reset counting to last item
-    itemNumber = songs.length -1;
-  }  
-  
-  // display data based on array index
-  songTitle.innerHTML = songs[itemNumber].title;
-  artist.innerHTML = songs[itemNumber].artist;
-  albumCover.style.backgroundImage = songs[itemNumber].image;
-  bgrdColor.style.backgroundColor = songs[itemNumber].color;
-  currentSong.src = songs[itemNumber].song;
-  currentSong.play();
-  record.style.transform = "rotate("+0+"deg)"
-  }
-  
-  // Random Spacebar
-  if ( event.key === " ") {
-    
-    // generate random number, for total number, of items (data)
-    randomNumber = Math.floor ( Math.random() * songs.length );
+    // update, then increase item number with each click
+    itemNumber = itemNumber + 1;
 
-    // display data based random number
-    songTitle.innerHTML = songs[randomNumber].title;
-    artist.innerHTML = songs[randomNumber].artist;
-    albumCover.style.backgroundImage = songs[randomNumber].image;
+    // if at LAST item
+    if (itemNumber > songs.length - 1) {
+      // reset counting to first item
+      itemNumber = 0;
+    }
+
+    // display data based on array index
+    songTitle.innerHTML = songs[itemNumber].title;
+    artist.innerHTML = songs[itemNumber].artist;
+    albumCover.style.backgroundImage = songs[itemNumber].image;
     bgrdColor.style.backgroundColor = songs[itemNumber].color;
     currentSong.src = songs[itemNumber].song;
     currentSong.play();
-    record.style.transform = "rotate("+0+"deg)"
-  
+    record.style.transform = "rotate(" + 0 + "deg)"
+
   }
-  
+
+  // Prev Left Arrow
+  if (event.key === "ArrowLeft") {
+
+    // update, then DEcrease item number with each click
+    itemNumber = itemNumber - 1;
+
+    // if at FIRST item
+    if (itemNumber < 0) {
+      // reset counting to last item
+      itemNumber = songs.length - 1;
+    }
+
+    // display data based on array index
+    songTitle.innerHTML = songs[itemNumber].title;
+    artist.innerHTML = songs[itemNumber].artist;
+    albumCover.style.backgroundImage = songs[itemNumber].image;
+    bgrdColor.style.backgroundColor = songs[itemNumber].color;
+    currentSong.src = songs[itemNumber].song;
+    currentSong.play();
+    record.style.transform = "rotate(" + 0 + "deg)"
+  }
+
+  // Random Spacebar
+  if (event.key === " ") {
+
+    // generate random number, for total number, of items (data)
+    randomNumber = Math.floor(Math.random() * songs.length);
+
+    // display data based random number
+    songTitle.innerHTML = songs[itemNumber].title;
+    artist.innerHTML = songs[itemNumber].artist;
+    albumCover.style.backgroundImage = songs[itemNumber].image;
+    bgrdColor.style.backgroundColor = songs[itemNumber].color;
+    currentSong.src = songs[itemNumber].song;
+    currentSong.play();
+    record.style.transform = "rotate(" + 0 + "deg)"
+
+  }
+
 });
 setInterval(recordRotate, 10)
